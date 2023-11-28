7610be501853f307bacfa9e0e1b849360b26f95c
sheifalisiddiq <sheif111@gmail.com>
2023-11-28 10:42:05 +0400

    broken_pirate.py file edited

--- broken_pirate.py
+++ broken_pirate.py
@@ -1,5 +1,5 @@
-greeting = input("Hello, possible pirate! What's the password?)
-if greeting in ["Arrr!"):
-	print("Go away, pirate.")
-elif
-print("Greetings, hater of pirates!")
+greeting = input("Hello, possible pirate! What's the password?")#added double quotes to correct syntax
+if greeting in ["Arrr!"]:#changed a closing bracket to square bracket
+    print("Go away, pirate.")
+else:# changed "elif" to "else" and added colon
+    print("Greetings, hater of pirates!")
